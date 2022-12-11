48901b7bd1a79a5b88c706c91e0c981ea318af1a
spences10 <spences10apps@gmail.com>
2022-12-11 21:31:07 +0000

    refactor: :recycle: update with Svelte

---
+++ src/lib/components/pay-reckoner.svelte
@@ -0,0 +1,8 @@
+<script>
+	// 260 working days in a year
+	let days = 260;
+	let salary = 35000;
+	let hours = 7.5;
+</script>
+
+<span>{salary / days / hours / 60 / 1000}</span>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import PayReckoner from '$lib/components/pay-reckoner.svelte';
+	import Stopwatch from '$lib/components/stopwatch.svelte';
+</script>
+
+<Stopwatch />
+<PayReckoner />
